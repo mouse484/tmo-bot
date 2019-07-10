f557d5da697cf809cfc41830c18d2d9b5a04467a
mouse_484 <escmouse484@gmail.com>
2019-07-10 00:01:02 +0000

    upgrade log

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log("まめのき🤔");
-    client.user.setActivity("Thinking Mamenoki Online")
+    client.user.setActivity("Thinking Mamenoki Online");
 });
 
 client.on("message", message => {
     if (message.author.id === "491418194762792961") {
+
         message.react("🤔");
-        client.channels.get("597206326602498068").send(`${message.cleanContent} : ${message.channel}`);
+
+        if (message.content.match(/(::|--)[a-z]/)) return;
+
+        client.channels.get("597206326602498068")
+            .send(message.cleanContent, {
+                embed: {
+                    fields: [
+                        {
+                            name: "チャンネル",
+                            value: `<#${message.channel.id}>`,
+                            inline: true,
+                        },
+                        {
+                            name: "リンク",
+                            value: `[まめのきさんの発言はこちら](${message.url})`,
+                            inline: true,
+                        }
+                    ],
+                    timestamp: new Date(),
+                },
+            });
     }
 });
 
