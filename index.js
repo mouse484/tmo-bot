6e1d48f73804133860180317d6f9d3b26441f476
mouse_484 <escmouse484@gmail.com>
2019-07-10 07:40:07 +0000

    メンションカウント

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const setting = {
-    target_id: "491418194762792961",
-    mame_channel_id: "597206326602498068",
-}
 
 client.on("ready", () => {
     console.log("まめのき🤔");
@@ -13,31 +9,41 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
-    if (message.author.id === setting.target_id) {
+    if (message.author.bot) return;
+
+    const mamenoki = client.users.get("491418194762792961");
+    const mame_channel = client.channels.get("597206326602498068")
+
+    if (message.author.id === mamenoki.id) {
 
         message.react("🤔");
 
         if (message.content.match(/(::|--)[a-z]/)) return;
 
-        client.channels.get(setting.mame_channel_id)
-            .send(message.cleanContent, {
-                embed: {
-                    fields: [
-                        {
-                            name: "チャンネル",
-                            value: `<#${message.channel.id}>`,
-                            inline: true,
-                        },
-                        {
-                            name: "リンク",
-                            value: `[まめのきさんの発言はこちら](${message.url})`,
-                            inline: true,
-                        }
-                    ],
-                    timestamp: new Date(),
-                },
-            });
+        mame_channel.send(message.cleanContent, {
+            embed: {
+                fields: [
+                    {
+                        name: "チャンネル",
+                        value: `<#${message.channel.id}>`,
+                        inline: true,
+                    },
+                    {
+                        name: "リンク",
+                        value: `[まめのきさんの発言はこちら](${message.url})`,
+                        inline: true,
+                    }
+                ],
+                timestamp: new Date(),
+            },
+        });
+    }
+
+    if (message.isMemberMentioned(mamenoki)) {
+        const value = mame_channel.name.split("mamenokiログ-")[1];
+        const num = Number(value) + 1;
+        mame_channel.setName(`mamenokiログ-${num}`);
     }
 });
 
-client.login(process.env.TOKEN);
+client.login("NTk4MTIyNzY5MjU0ODQyMzc4.XSWR_w.hooWrFyYQo03ZpTwpcirzadrbVA");
