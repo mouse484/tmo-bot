9d05470b364e5f9111b5bf69c5a96dbcb1ee6e9e
mouse_484 <escmouse484@gmail.com>
2019-07-13 05:25:06 +0000

    emohis

--- src/thinking.js
+++ src/thinking.js
@@ -7,4 +7,7 @@ module.exports = (client, message) => {
     message.react(client.emojis.get("599466988061982740"));
     message.react(client.emojis.get("599462853614895135"));
     message.react(client.emojis.get("599462875899101206"));
+    message.react(client.emojis.get("599468434656329738"));
+    message.react(client.emojis.get("599470217948823552"));
+    message.react(client.emojis.get("599470644270465065"));
 }
